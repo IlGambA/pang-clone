c69ee5b61d320bacfa9045a7e51c9cbdffcdbfe9
Stefano Gambarin <gambarin.stefano@gmail.com>
2025-08-28 11:58:31 +0200

    Fix Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,54 +3,54 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody) , typeof(Animator))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float force = 1f;
+    [SerializeField] private float _force;
     
-    private Animator playerAnimator;
-    private Rigidbody playerRigidBody;
+    private Animator _playerAnimator;
+    private Rigidbody _playerRigidBody;
 
-    private bool isWalk;
-    private int direction;
+    private bool _isWalk;
+    private int _direction;
     
-    private static readonly int Player_Animator_Walk = Animator.StringToHash("isWalk");
-    private static readonly int Player_Animator_Direction = Animator.StringToHash("Direction");
+    private static readonly int PlayerAnimatorWalk = Animator.StringToHash("isWalk");
+    private static readonly int PlayerAnimatorDirection = Animator.StringToHash("Direction");
     private void Awake()
     {
-        playerAnimator = GetComponent<Animator>();
-        if (playerAnimator is null)
+        _playerAnimator = GetComponent<Animator>();
+        if (_playerAnimator is null)
         {
             Debug.LogError("PlayerAnimator is null");
         }
-        playerRigidBody = GetComponent<Rigidbody>();
-        if (playerRigidBody is null)
+        _playerRigidBody = GetComponent<Rigidbody>();
+        if (_playerRigidBody is null)
         {
             Debug.LogError("PlayerRigidBody is null");
         }
-        isWalk = false;
-        direction = 0;
-        playerRigidBody.maxLinearVelocity = 2f;
+        _isWalk = false;
+        _direction = 0;
+        _playerRigidBody.maxLinearVelocity = 3f;
     }
 
     private void Update()
     {
-        Debug.Log(playerRigidBody.linearVelocity);
+        Debug.Log(_playerRigidBody.linearVelocity);
     }
     private void FixedUpdate()
     {
-        isWalk = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D);
-        playerAnimator.SetBool(Player_Animator_Walk, isWalk);
+        _isWalk = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D);
+        _playerAnimator.SetBool(PlayerAnimatorWalk, _isWalk);
         
-        
-        if (isWalk && Input.GetKey(KeyCode.A))
+        if (_isWalk && Input.GetKey(KeyCode.A))
         {
-            direction = 1;
-            playerRigidBody.AddForce(Vector3.left * force,ForceMode.VelocityChange);
-            playerAnimator.SetInteger(Player_Animator_Direction, direction);
+            _direction = -1;
+            _playerAnimator.SetInteger(PlayerAnimatorDirection, _direction);
+            _playerRigidBody.AddForce(Vector3.left * _force,ForceMode.Acceleration);
         }
-        else if (isWalk && Input.GetKey(KeyCode.D))
+        if (_isWalk && Input.GetKey(KeyCode.D))
         {
-            direction = 0;
-            playerRigidBody.AddForce(Vector3.right * force,ForceMode.VelocityChange);
-            playerAnimator.SetInteger(Player_Animator_Direction, direction);
+            _direction = 1;
+            _playerAnimator.SetInteger(PlayerAnimatorDirection, _direction);
+            _playerRigidBody.AddForce(Vector3.right * _force,ForceMode.Acceleration);
+
         }
         
         
