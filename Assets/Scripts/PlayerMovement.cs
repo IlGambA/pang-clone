b82852aa183865ef102925f9731029c8714d3c3c
Stefano Gambarin <gambarin.stefano@gmail.com>
2025-08-28 12:04:20 +0200

    refactor player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody) , typeof(Animator))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float _force;
+    [SerializeField] private float force;
     
     private Animator _playerAnimator;
     private Rigidbody _playerRigidBody;
@@ -15,25 +15,25 @@ public class PlayerMovement : MonoBehaviour
     private static readonly int PlayerAnimatorDirection = Animator.StringToHash("Direction");
     private void Awake()
     {
+        _isWalk = false;
+        _direction = 0;
+        
         _playerAnimator = GetComponent<Animator>();
         if (_playerAnimator is null)
         {
             Debug.LogError("PlayerAnimator is null");
+            return;
         }
         _playerRigidBody = GetComponent<Rigidbody>();
+        
         if (_playerRigidBody is null)
         {
             Debug.LogError("PlayerRigidBody is null");
+            return;
         }
-        _isWalk = false;
-        _direction = 0;
         _playerRigidBody.maxLinearVelocity = 3f;
     }
-
-    private void Update()
-    {
-        Debug.Log(_playerRigidBody.linearVelocity);
-    }
+    
     private void FixedUpdate()
     {
         _isWalk = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D);
@@ -43,16 +43,14 @@ public class PlayerMovement : MonoBehaviour
         {
             _direction = -1;
             _playerAnimator.SetInteger(PlayerAnimatorDirection, _direction);
-            _playerRigidBody.AddForce(Vector3.left * _force,ForceMode.Acceleration);
+            _playerRigidBody.AddForce(Vector3.left * force,ForceMode.Acceleration);
         }
         if (_isWalk && Input.GetKey(KeyCode.D))
         {
             _direction = 1;
             _playerAnimator.SetInteger(PlayerAnimatorDirection, _direction);
-            _playerRigidBody.AddForce(Vector3.right * _force,ForceMode.Acceleration);
+            _playerRigidBody.AddForce(Vector3.right * force,ForceMode.Acceleration);
 
         }
-        
-        
     }
 }
