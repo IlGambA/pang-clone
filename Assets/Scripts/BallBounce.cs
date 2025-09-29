fae9efa05761d4c32b2249f732ee587f2ff0ce80
Stefano Gambarin <gambarin.stefano@gmail.com>
2025-09-29 11:16:28 +0200

    Destroy ball when hit

--- Assets/Scripts/BallBounce.cs
+++ Assets/Scripts/BallBounce.cs
@@ -2,51 +2,161 @@ using UnityEngine;
 
 public class BallBounce : MonoBehaviour
 {
-    [Tooltip("widen or narrow the distance between the two rebounds")]
-    [SerializeField] private float amplitude = 1f;
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float gravity = -15f;
+    [SerializeField] private float maxHeight = 10f;
     
-    [Tooltip("Change force bounce")]   
-    [SerializeField] private float force = 1f;
+    [Header("Ball Splitting")]
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private float splitSpeed = 6f;
+    [SerializeField] private bool canSplit = true; 
     
-    private Rigidbody rb;
-    private float direction;
-    private Vector3 currentPosition;
-    
-    
-    private void Awake()
+    private Vector3 velocity;
+    private float startY;
+    private bool _invulnerable = false;
+    private float _timeInvulnerable = 1f;
+    private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        if (rb is null)
-        {
-            Debug.LogError(this.name + ": needs a Rigidbody");
-        }
-        direction = 1f;
+        velocity = new Vector3(speed, speed, 0f);
+        startY = transform.position.y;
+      
     }
-
-    private void OnCollisionEnter(Collision collision)
+    
+    private void Update()
     {
-        // Invert direction when collide with wall
-        if (collision.collider.CompareTag("Wall"))
+        if (_invulnerable)
         {
-            direction = -direction;
+            _timeInvulnerable -= Time.deltaTime;
+            if ( _timeInvulnerable < 0 )
+            {
+                _invulnerable = false;
+            }
         }
         
-        // Check if collide with ground
-        if (collision.collider.CompareTag("Ground"))
+        transform.position += velocity * Time.deltaTime;
+        velocity.y += gravity * Time.deltaTime;
+        ClampHeight();
+    }
+    
+    void ClampHeight()
+    {
+        float currentY = transform.position.y;
+        float maxAllowedY = startY + maxHeight;
+        
+        if (currentY > maxAllowedY)
         {
-            Debug.Log(gameObject.name + " collided with " + collision.collider.name);
-
-            //Get Normal
-            Vector3 collisionNormal = collision.contacts[0].normal;
-            Debug.DrawRay(collision.contacts[0].point, collisionNormal.normalized * 3, Color.red, 2f);
-
-            //Add force for bounce
-            rb.AddForce(collisionNormal * force, ForceMode.Impulse);
+            transform.position = new Vector3(transform.position.x, maxAllowedY, transform.position.z);
+            velocity.y = -Mathf.Abs(velocity.y);
+            Debug.Log("Hit max height!");
         }
     }
-    private void FixedUpdate()
+    
+    private void OnTriggerEnter(Collider other)
     {
-        //Move ball along x position based of direction and amplitude physically
-        rb.AddForce(Vector3.right * (amplitude*direction), ForceMode.Force);
+        if (other.CompareTag("Wall"))
+        {
+            BounceHorizontal();
+        }
+        else if (other.CompareTag("Ground"))
+        {
+            BounceVertical();
+        }
+        else if (other.CompareTag("Obstacle"))
+        {
+            BounceFromObstacle(other);
+        }
+        else if (other.CompareTag("Rope"))
+        {
+            if(_invulnerable)return;
+            DestroyAndSplit();
+        }
+    }
+    
+    void BounceHorizontal()
+    {
+        velocity.x = -velocity.x;
+        Debug.Log("Bounce horizontal");
+    }
+    
+    void BounceVertical()
+    {
+        velocity.y = Mathf.Abs(velocity.y);
+        if (velocity.y < speed * 0.5f)
+        {
+            velocity.y = speed * 0.8f;
+        }
+        Debug.Log("Bounce vertical");
+    }
+    
+    void BounceFromObstacle(Collider obstacle)
+    {
+        Vector3 ballPos = transform.position;
+        Vector3 obstaclePos = obstacle.transform.position;
+        
+        float deltaX = ballPos.x - obstaclePos.x;
+        float deltaY = ballPos.y - obstaclePos.y;
+        
+        if (Mathf.Abs(deltaX) > Mathf.Abs(deltaY))
+        {
+            BounceHorizontal();
+        }
+        else
+        {
+            velocity.y = -velocity.y;
+        }
+        
+        Debug.Log("Bounce from obstacle");
+    }
+    
+    void DestroyAndSplit()
+    {
+        Debug.Log("Ball destroyed by rope!");
+        
+        if (canSplit && ballPrefab != null)
+        {
+            SpawnChildBalls();
+        }
+        
+        Destroy(gameObject);
+    }
+    
+    void SpawnChildBalls()
+    {
+        Vector3 currentPos = transform.position;
+        
+        // Left Ball
+        GameObject leftBall = Instantiate(ballPrefab, currentPos, Quaternion.identity);
+        BallBounce leftScript = leftBall.GetComponent<BallBounce>();
+        
+        if (leftScript != null)
+        {
+            leftScript.SetSpeed(-splitSpeed);
+            leftScript.maxHeight = maxHeight; 
+            leftScript.startY = currentPos.y;
+            leftScript._invulnerable = true;
+        }
+        
+        // Right Ball
+        GameObject rightBall = Instantiate(ballPrefab, currentPos, Quaternion.identity);
+        BallBounce rightScript = rightBall.GetComponent<BallBounce>();
+        if (rightScript != null)
+        {
+            rightScript.SetSpeed(splitSpeed);
+            rightScript.maxHeight = maxHeight;
+            rightScript.startY = currentPos.y;
+            rightScript._invulnerable = true;
+        }
+        
+        Debug.Log("Spawned 2 child balls!");
+    }
+    
+    private void SetSpeed(float _speed)
+    {
+        speed = _speed;
+    }
+    private void SetMaxHeight(float _maxHeight)
+    {
+        maxHeight = _maxHeight;
     }
 }
